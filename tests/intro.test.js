4d0f38abcb483542f6126259bda58083bd7dc2f0
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-22 16:35:32 -0400

    Add FizzBuzz test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { max } from '../src/intro';
+import { fizzBuzz, max } from '../src/intro';
 
 describe('Max', () => {
     it('should return the first argument if it is greater', () => {
@@ -12,5 +12,23 @@ describe('Max', () => {
 
     it('should return the first argument if they are equal', () => {
         expect(max(1, 1)).toBe(1);
+    })
+})
+
+describe('FizzBuzz', () => {
+    it('should return FizzBuzz if its divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+
+    it('should return Fizz if its divisible by 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    })
+
+    it('should return Buzz if its divisible by 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+    })
+
+    it('should return the same number if its not divisible by either 3 or 5', () => {
+        expect(fizzBuzz(1)).toBe('1');
     })
 })
