bfa47be9e3fe63388329e33ad9f806a044d8e4fa
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-22 16:35:39 -0400

    Refactor fizzbuzz

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,15 @@
 // Lesson: Writing your first tests
 export const max = (a, b) => (a > b) ? a : b;
 
-
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (isDivisibleBy(n, 15)) return 'FizzBuzz';
+  if (isDivisibleBy(n, 3)) return 'Fizz';
+  if (isDivisibleBy(n, 5)) return 'Buzz';
+  
   return n.toString();
 }
+
+function isDivisibleBy(number, divisor) {
+  return number % divisor === 0;
+} 
