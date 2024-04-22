dd14bc2c2e5ee0d1383e815bf1c45b08697814fa
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-22 16:23:26 -0400

    Add max tests

---
+++ tests/intro.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+import { max } from '../src/intro';
+
+describe('Max', () => {
+    it('should return the first argument if it is greater', () => {
+        expect(max(2, 1)).toBe(2);
+    })
+
+    it('should return the second argument if it is greater', () => {
+        expect(max(1, 2)).toBe(2);
+    })
+
+    it('should return the first argument if they are equal', () => {
+        expect(max(1, 1)).toBe(1);
+    })
+})
