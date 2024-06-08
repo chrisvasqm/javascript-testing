fe26e2612e055fe10c8bfb75c8602bf73d8906cf
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-08 16:15:35 -0400

    Add ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,8 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
+
 
 export default [
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: globals.node
-    }
-  }
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
